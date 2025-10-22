81ee3c8441b199c0d9e4fa4c36b784991dcf7cae
Srushti Basavaraju <srushti.basavaraju@rothbury.co.nz>
2025-10-22 20:12:35 +1300

    wrote my first docker-compose.yml file

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Hey, I am node.js in container'});
+app.get("/", (req, res) => {
+     res.json( {message: "Hey, I am node.js in container"} );
 });
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
